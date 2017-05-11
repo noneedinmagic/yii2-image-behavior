03eea629e501ee45ab43673a1f97fcccefeeb8ad
Oleg <noneedinmagic@gmail.com>
2017-05-11 04:39:23 +0300

    Initial ImageBehavior code taken from the previous projects

---
+++ OptionalImageBehavior.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace noneedinmagic\images;
+
+use Yii;
+
+class OptionalImageBehavior extends ImageBehavior
+{
+	public function beforeValidate($event){
+		parent::beforeValidate($event);
+		return TRUE;
+	}
+
+	public function beforeSave($event){
+		return empty($this->owner->{$this->fileAttribute}) || parent::beforeSave($event);
+	}
+
+	public function afterDelete($event){
+		if(!empty($this->owner->{$this->filenameAttribute})){
+			parent::afterDelete($event);
+		}
+	}
+
+    public function imageUrl($options = [])
+    {
+        return empty($this->owner->{$this->filenameAttribute}) ? NULL : parent::imageUrl($options);
+    }
+
+    public function imageOriginalPath()
+    {
+        return empty($this->owner->{$this->filenameAttribute}) ? NULL : parent::imageOriginalPath();
+    }
+
+    protected function imageCachePath()
+    {
+        return empty($this->owner->{$this->filenameAttribute}) ? NULL : parent::imageCachePath();
+    }
+
+    public function dropCache()
+    {
+		if(!empty($this->owner->{$this->filenameAttribute})){
+			parent::dropCache();
+		}
+    }
+
+    public function imagePath($options = []){
+        return empty($this->owner->{$this->filenameAttribute}) ? NULL : parent::imagePath($options);
+    }
+}
